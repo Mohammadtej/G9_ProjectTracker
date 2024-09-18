608900d8166ff23c6ab309c05a8fcf7eac00cb65
Mohammadtej <202001406@daiict.ac.in>
2024-09-18 16:49:08 +0530

    Add bean for fetching pm id and filtering the projects

---
+++ ProjectTrackerG9/ViewController/src/view/TaskFlowManagedBean.java
@@ -0,0 +1,41 @@
+package view;
+
+import oracle.adf.share.ADFContext;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.jbo.ViewObject;
+
+@ManagedBean(name = "taskFlowManagedBean")
+@RequestScoped
+public class TaskFlowManagedBean {
+    public TaskFlowManagedBean() {
+        super();
+    }
+    
+    public Integer getCurrentUser() {
+        String userName = ADFContext.getCurrent().getSecurityContext().getUserName();
+        System.out.println("In the managed bean" + userName);
+        try {
+            return Integer.parseInt(userName);  // Convert to Integer
+        } catch (NumberFormatException e) {
+            // Handle exception in case userName is not a number
+            return null;  // Or return a default value, e.g., -1
+        }
+    }
+    
+    public void applyFilterToViewObject() {
+            // Get the current Application Module's DataControl
+            DCBindingContainer bindings = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();
+    
+            // Find the View Object by its iterator binding name (replace with your actual VO iterator name)
+            ViewObject viewObject = bindings.findIteratorBinding("Projects1Iterator").getViewObject();
+    
+            // Set the bind variable with the value from getCurrentUser
+            viewObject.setNamedWhereClauseParam("retrievedId", getCurrentUser());
+    
+            // Execute the query
+            viewObject.executeQuery();
+    }
+}
